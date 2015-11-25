afd0d800d5711aabfed1f4ae667cced091ac7ecb
qusiba <chenrunshan@outlook.com>
2015-11-25 13:50:01 +0800

    xxxZZ

--- src/js/lib/pay.js
+++ src/js/lib/pay.js
@@ -2,7 +2,7 @@ import {wxpay_api} from 'api';
 import $ from 'jq';
 
 let doWxpay = function (orderId, openId, cb) {
-	cb = cb || function () { msg('nothing callback...') };
+	cb = cb || function () {alert('nothing callback...') };
 	let _appId, _timeStamp, _nonceStr, _package, _paySign = '';
 	$.get(wxpay_api, { orderId: orderId, openId: openId }, function (data) {
 		_appId = data.appId;
@@ -13,7 +13,7 @@ let doWxpay = function (orderId, openId, cb) {
 		WeixinJSBridge.invoke('getBrandWCPayRequest', {
 			"appId": _appId, "timeStamp": _timeStamp, "nonceStr": _nonceStr, "package": _package, "signType": "MD5", "paySign": _paySign
 		}, function (res) {
-			msg(res.err_msg);
+			alert(res.err_msg);
 			if (res.err_msg == "get_brand_wcpay_request:ok") {
 				cb();
 			} else {
