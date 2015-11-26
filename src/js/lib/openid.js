9bc56fc49ac1423cf7ac3ef0db890e2de0a6d678
qusiba <chenrunshan@outlook.com>
2015-11-26 15:19:26 +0800

    cookie

--- src/js/lib/openid.js
+++ src/js/lib/openid.js
@@ -1,6 +1,8 @@
 import {getQuery,cookie} from 'func';
-let openId = cookie.get('openId') || getQuery('openid');
-cookie.set('openId', openId, 30);
+let openId = getQuery('openid') || cookie.get('openId');
+if(openId){
+	cookie.set('openId', openId, 30);
+}
 if(!openId){
 	alert("不能获取微信授权信息，正在为您重新授权");
 }
