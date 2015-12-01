80606988f9376030320dcefd7b33d669f8b33b24
qusiba <chenrunshan@outlook.com>
2015-12-02 00:35:55 +0800

    go

--- src/1212/main.js
+++ src/1212/main.js
@@ -73,10 +73,8 @@ $(function(){
   });
 
   var hei = $('.action').innerHeight();
-  console.log(hei)
   $(window).scroll(function(){
-    //console.log(hei)
-   var  top = $(document.body).scrollTop();
+    var top = $(document.body).scrollTop();
     if(top >= hei){
       $('.car_nav').addClass('car_nav_fix');
     }else{
@@ -84,18 +82,11 @@ $(function(){
     }
   });
   var smart = getQuery('smartcode');
-  if(isWx){
-    $('.j-get').attr('href','http://weixin.dongfeng-nissan.com.cn/huodong/index.php?a=DoubleTwelve&m=index&key=N-Weixin-Wap-V4-Ac-Le-PoC-Msg2-02-20151201-1212');
-    if(typeof(smart)!= undefined && smart != null) {
-      $('.j-get').attr('href','http://weixin.dongfeng-nissan.com.cn/huodong/index.php?a=DoubleTwelve&m=index&key=N-Weixin-Wap-V4-Ac-Le-PoC-Msg2-02-20151201-1212&smartCode='+smart);
-    }
-  }else {
-      $('.j-get').attr('href','http://weixin.dongfeng-nissan.com.cn/huodong/index.php?a=DoubleTwelve&m=index&key=N-Chebaba-Wap-V4-Ac-Le-PoC-Msg30-01-20151201-1212');
-    if(typeof(smart)!= undefined && smart != null) {
-      $('.j-get').attr('href','http://weixin.dongfeng-nissan.com.cn/huodong/index.php?a=DoubleTwelve&m=index&key=N-Weixin-Wap-V4-Ac-Le-PoC-Msg2-02-20151201-1212&smartCode='+smart);
-    }
-  }
-
-
+  $('.j-get').attr('href',
+    'http://weixin.dongfeng-nissan.com.cn/huodong/index.php?a=DoubleTwelve&m=index&key=' 
+      + isWx ? 'N-Weixin-Wap-V4-Ac-Le-PoC-Msg2-02-20151201-1212' : 'N-Chebaba-Wap-V4-Ac-Le-PoC-Msg30-01-20151201-1212'
+      + (smart ? '&smartCode=' + smart : ''));
+  //微信分享设置
+  if (isWx) { require('share')("买全新逍客送半税，购全新楼兰，蓝鸟三年养车不花钱，速来！", CONFIG.CONTEXT_PATH + '/topic/1212/img/ac_01.jpg', 'Nissan 新车大赏', location.href); }
 
 });
