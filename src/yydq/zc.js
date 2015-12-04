14e9969c9b5474e8ab71ce9a95cddd2d0cea184b
qusiba <chenrunshan@outlook.com>
2015-12-04 17:51:54 +0800

    wx auth

--- src/yydq/zc.js
+++ src/yydq/zc.js
@@ -1,9 +1,8 @@
+import {getQuery, isWx, formatTime} from 'func';
+!getQuery(openId) && (location.href = 'http://dfldata-dev.dongfeng-nissan.com.cn/weixin/Nissan/www/api.php?a=E4S&m=userOauth&url=' + location.href);
 require('./zc.less');
 import $ from 'jq';
-import {getQuery, isWx, formatTime} from 'func';
 import {city_api, store_api, zc_pay, zc_count, clue_api, vcode_api} from 'api';
-
-
 $(function () {
 	//引入弹框插件
 	let layer = require('layer');
